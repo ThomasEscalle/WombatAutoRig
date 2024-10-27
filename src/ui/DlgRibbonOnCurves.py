6d8f08a835e57c75b8768562fa76c1c03d9f15d1
Calimer01 <mthcarrey750@gmail.com>
2024-10-27 18:09:43 +0100

    Create DlgRibbonOnCurves.py

---
+++ src/ui/DlgRibbonOnCurves.py
@@ -0,0 +1,42 @@
+from PySide2 import QtWidgets, QtCore, QtGui
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+import sys
+from PySide2 import QtWidgets, QtCore, QtGui
+import maya.OpenMayaUI as omui
+import shiboken2
+from maya.app.general.mayaMixin import MayaQWidgetDockableMixin
+import maya.cmds as cmds
+from maya.OpenMayaUI import MQtUtil
+
+from wombatAutoRig.src.ui.forms import ui_RibbonOnCurves
+from wombatAutoRig.src.ui import IconLoader
+from wombatAutoRig.src.core import FileHelper
+
+from wombatAutoRig.src.core import Color
+
+def maya_main_window():
+    main_window_ptr = omui.MQtUtil.mainWindow()
+    return shiboken2.wrapInstance(int(main_window_ptr), QtWidgets.QWidget)
+
+
+# Classe de la fenêtre de création de template
+class RibbonOnCurve(MayaQWidgetDockableMixin, QtWidgets.QDialog):
+    def __init__(self, parent=maya_main_window()):
+        super(RibbonOnCurve, self).__init__(parent)
+        self.setWindowFlags(self.windowFlags() ^ QtCore.Qt.WindowContextHelpButtonHint)
+
+        self.ui = ui_RibbonOnCurves.Ui_RibbonOnCurve()
+        self.ui.setupUi(self)
+
+        self.setWindowTitle("Ribbon On Curves")
+
+
+
+
+    # Show window with docking ability
+    def run(self):
+        self.show(dockable = True)
+
