1068eff0bbb39be8242339042b3bf83d801094af
Calimer01 <mthcarrey750@gmail.com>
2024-11-09 13:57:34 +0100

    trie

--- src/core/JointPlacementTest.py
+++ src/core/JointPlacementTest.py
@@ -29,40 +29,6 @@ def connectLine(object0, object1, name="line", width = 4 ):
 groupJointPlacement = cmds.group(name='JointPlacement', empty=True)
 groupLineJointPlacement = cmds.group(name='LineJointPlacement', empty=True)
 
-#AIMCONSTRAINT MODE
-
-##CREATING THE BOUBOUL
-#Ankle_L = JointPlacement.createController(pos=(4.125,9.609,-0.318),name='Ankle_L')
-#Ball_L = JointPlacement.createController(pos=(4.366,2.707,4.391),name='Ball_L')
-#Toe_L = JointPlacement.createController(pos=(4.411,1.428,8.568),name='Toe_L')
-#Knee_L = JointPlacement.createController(pos=(4.299,28.693,0.09),name='Knee_L')
-#Leg_L = JointPlacement.createController(pos=(4.46,46.985,0), name='Leg_L')
-#Root = JointPlacement.createController(pos=(0,52,0), localRotation = [0,0,90], name='Root')
-
-###LINE
-#Line0 = connectLine(Leg_L, Knee_L)
-#Line1 = connectLine(Knee_L, Ankle_L)
-#Line2 = connectLine(Ankle_L, Ball_L)
-#Line3 = connectLine(Ball_L, Toe_L)
-#Line4 = connectLine(Root, Leg_L)
-#
-##RANGEMENT
-#cmds.parent('Ankle_L','Ball_L','Toe_L','Knee_L', Leg_L, Root, groupJointPlacement)
-#cmds.parent( Line0, Line1, Line2, Line3, Line4, groupLineJointPlacement)
-#Offset.offset(Toe_L, nbr=1)
-#Offset.offset(Ball_L, nbr=1)
-#Offset.offset(Ankle_L, nbr=1)
-#Offset.offset(Knee_L, nbr=1)
-#Offset.offset(Leg_L, nbr=1)
-#Offset.offset(Root, nbr=1)
-
-
-##AIMCONSTRAINT
-#cmds.aimConstraint(Ball_L,Ankle_L, wu=(1,0,0))
-#cmds.aimConstraint(Ankle_L,Knee_L, wu=(1,0,0))
-#cmds.aimConstraint(Toe_L,Ball_L, wu=(1,0,0))
-#cmds.aimConstraint(Knee_L,Leg_L, wu=(1,0,0))
-#cmds.connectAttr(Ball_L+'.r', Toe_L+'.r')
 
 
 
@@ -104,6 +70,7 @@ Line4 = connectLine(Root, Leg_L)
 
 #RANGEMENT
 cmds.parent( Line0, Line1, Line2, Line3, Line4, groupLineJointPlacement)
+cmds.parent(Root, groupJointPlacement)
 Offset.offset(Toe_L, nbr=1)
 Offset.offset(Ball_L, nbr=1)
 Offset.offset(Ankle_L, nbr=1)
@@ -111,5 +78,6 @@ Offset.offset(Knee_L, nbr=1)
 Offset.offset(Leg_L, nbr=1)
 Offset.offset(Root, nbr=1)
 
+#MATCHING PIVOT
+cmds.matchTransform(Knee_L, Leg_L, piv=True)
 
-cmds.parent(Root, groupJointPlacement)
