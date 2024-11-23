ed3ce568ce6468bd9274fce5885f9610ddb4dfe5
ThomasEscalle <60444618+ThomasEscalle@users.noreply.github.com>
2024-11-23 18:33:31 +0100

    qzd
    zd

--- templates/Body/placement_joints.py
+++ templates/Body/placement_joints.py
@@ -18,13 +18,23 @@ def createJoint(name, position, orientation, settings):
 
 
 def placeJointsLegs(settings):
-    #Creating Joints for the legs
+    # Creating the root joint
     createJoint("PlacementJnt_Root", (0, 97.699, 0), (180,0,90), settings)
+
+    # Creating Joints for the legs left
     createJoint("PlacementJnt_Hip_L", (7.636, 88.219, 0), (0,-1.767,-90), settings)
     createJoint("PlacementJnt_Knee_L", (7.636, 47.195, 1.265), (0,2.092,-90), settings)
     createJoint("PlacementJnt_Ankle_L", (7.636, 6.223, -0.231), (0,-80,-90), settings)
     createJoint("PlacementJnt_Ball_L", (7.636, 4.696, 8.428), (-90,-90,0), settings)
     createJoint("PlacementJnt_Toe_L", (7.636, 4.696, 16.486), (-90,-90,0), settings)
+
+    # Creating Joints for the legs right
+    createJoint("PlacementJnt_Hip_R", (-7.636, 88.219, 0), (0,1.767,-90), settings)
+    createJoint("PlacementJnt_Knee_R", (-7.636, 47.195, 1.265), (0,-2.092,-90), settings)
+    createJoint("PlacementJnt_Ankle_R", (-7.636, 6.223, -0.231), (0,80,-90), settings)
+    createJoint("PlacementJnt_Ball_R", (-7.636, 4.696, 8.428), (-90,90,0), settings)
+    createJoint("PlacementJnt_Toe_R", (-7.636, 4.696, 16.486), (-90,90,0), settings)
+
     
     #Parenting Joints for the legs
     cmds.parent("PlacementJnt_Hip_L", "PlacementJnt_Root")
@@ -44,6 +54,19 @@ def placeJointsLegs(settings):
     cmds.parent("PlacementCtrl_Foot_Int_L", "JointsPlacement")
     cmds.parent("PlacementCtrl_Foot_Ext_L", "JointsPlacement")
     cmds.parent("PlacementCtrl_Foot_Back_L", "JointsPlacement")
+
+
+    # Create the placement controllers for the foot (front, left, right, back)
+    JointPlacement.createControllerSphere((-7.636, 0, 16.905), sphereRadius, "PlacementCtrl_Foot_Front_R" )
+    JointPlacement.createControllerSphere((-2.969, 0, 8.428), sphereRadius, "PlacementCtrl_Foot_Int_R")
+    JointPlacement.createControllerSphere((-13.606, 0, 8.428), sphereRadius, "PlacementCtrl_Foot_Ext_R")
+    JointPlacement.createControllerSphere((-7.636, 0, -5.765), sphereRadius, "PlacementCtrl_Foot_Back_R")
+
+    cmds.parent("PlacementCtrl_Foot_Front_R", "JointsPlacement")
+    cmds.parent("PlacementCtrl_Foot_Int_R", "JointsPlacement")
+    cmds.parent("PlacementCtrl_Foot_Ext_R", "JointsPlacement")
+    cmds.parent("PlacementCtrl_Foot_Back_R", "JointsPlacement")
+    
 
     return 'PlacementJnt_Root'
         
