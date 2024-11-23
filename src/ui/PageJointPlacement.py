4a6585f0e029707655bd7dfa54924f096b0e026f
ThomasEscalle <60444618+ThomasEscalle@users.noreply.github.com>
2024-11-23 14:28:53 +0100

    Implemented the LRA Placement

--- src/ui/PageJointPlacement.py
+++ src/ui/PageJointPlacement.py
@@ -3,6 +3,10 @@ from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
+from wombatAutoRig.src.ui import IconLoader
+
+from maya import cmds
+
 from wombatAutoRig.src.ui.forms import ui_PageJointPlacement
 from wombatAutoRig.src.ui.PageBase import PageBase
 
@@ -13,6 +17,9 @@ class PageJointPlacement(PageBase):
         self.ui = ui_PageJointPlacement.Ui_PageJointPlacement()
         self.ui.setupUi(self)
 
+        self.ui.btnShowLRA.clicked.connect(self.showLraButtonClicked)
+        self.ui.btnShowLRA.setIcon(IconLoader.loadIcon("LRA"))
+
     # Say if the user can go to the next page or not
     def canGoNext(self):
         return True
@@ -20,3 +27,15 @@ class PageJointPlacement(PageBase):
     def addDataToSettings(self,settings):
         print("Add the data to the settings")
         return settings
+    
+    def showLraButtonClicked(self):
+        # Loop for all the joints in the scene
+        # If their prefix is "PlacementJnt" then show or hide the LRA
+        for jnt in cmds.ls(type="joint"):
+            if jnt.startswith("PlacementJnt_"):
+                # Check if the LRA is visible
+                if cmds.getAttr(jnt + ".displayLocalAxis"):
+                    cmds.setAttr(jnt + ".displayLocalAxis", 0)
+                else:
+                    cmds.setAttr(jnt + ".displayLocalAxis", 1)
+        return
--- src/ui/forms/ui_PageJointPlacement.py
+++ src/ui/forms/ui_PageJointPlacement.py
@@ -17,7 +17,7 @@ class Ui_PageJointPlacement(object):
     def setupUi(self, PageJointPlacement):
         if not PageJointPlacement.objectName():
             PageJointPlacement.setObjectName(u"PageJointPlacement")
-        PageJointPlacement.resize(400, 300)
+        PageJointPlacement.resize(723, 640)
         self.verticalLayout = QVBoxLayout(PageJointPlacement)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.label = QLabel(PageJointPlacement)
@@ -34,6 +34,11 @@ class Ui_PageJointPlacement(object):
 
         self.verticalLayout.addWidget(self.label_2)
 
+        self.btnShowLRA = QPushButton(PageJointPlacement)
+        self.btnShowLRA.setObjectName(u"btnShowLRA")
+
+        self.verticalLayout.addWidget(self.btnShowLRA)
+
         self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
 
         self.verticalLayout.addItem(self.verticalSpacer)
@@ -48,5 +53,6 @@ class Ui_PageJointPlacement(object):
         PageJointPlacement.setWindowTitle(QCoreApplication.translate("PageJointPlacement", u"Form", None))
         self.label.setText(QCoreApplication.translate("PageJointPlacement", u"Joints placement", None))
         self.label_2.setText("")
+        self.btnShowLRA.setText(QCoreApplication.translate("PageJointPlacement", u"Show LRA", None))
     # retranslateUi
 
