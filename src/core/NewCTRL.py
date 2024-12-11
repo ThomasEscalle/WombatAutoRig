42b6b3ff771a9081175490384534d16be9a181fe
Calimer01 <mthcarrey750@gmail.com>
2024-12-11 11:59:03 +0100

    Create NewCTRL.py

---
+++ src/core/NewCTRL.py
@@ -0,0 +1,26 @@
+import maya.cmds as cmds
+from wombatAutoRig.src.core import Offset
+
+def NewCTRL(CTRL, Joint, name)->str:
+
+    #Create a group to be the new CTRL
+    cmds.group(n=f"{name}", empty=True)
+    cmds.matchTransform(name, Joint, pos=True, rot=True)
+    Offset.offset(f"{name}", nbr=1)
+
+    #mettre le CTRL dans le bon worldSpace
+    cmds.parent(CTRL, f"{name}_Offset")
+
+    #parenter la shape du CTRL dans le new CTRL
+    cmds.makeIdentity(CTRL, a=True, t=True, r=True, s=True)
+    Shape = cmds.listRelatives(CTRL, s=True)
+    cmds.parent(Shape, f"{name}_Offset", s=True)
+
+    #Supprimer l'ancien CTRL
+    cmds.delete(CTRL)
+    cmds.delete(name)
+
+    cmds.rename("transform1", name)
+
+
+    return f"{name}_Offset"
