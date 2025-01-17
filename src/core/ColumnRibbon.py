277bd28031340eb69177cdd4fc9aafaa495d0961
Calimer01 <mthcarrey750@gmail.com>
2025-01-17 16:52:44 +0100

    Update ColumnRibbon.py

--- src/core/ColumnRibbon.py
+++ src/core/ColumnRibbon.py
@@ -1,5 +1,7 @@
 import maya.cmds as cmds
 from wombatAutoRig.src.core import Bookmark
+from wombatAutoRig.src.core import Offset
+from wombatAutoRig.src.core import Controllers
 
 
 def ColumnRibbon(name="Default", height=2):
@@ -73,6 +75,71 @@ def ColumnRibbon(name="Default", height=2):
     cmds.setAttr(MovablePivot + ".translateY", 7*height/8)
     cmds.parent(MovablePivot, CTRLIK)
     cmds.parent(CTRLIK, f"Ribbon_Spine_{name}")
+    Offset.Offset(CTRLIK, nbr=1)
+    cmds.connectAttr(MovablePivot + ".translate", CTRLIK[0] + ".rotatePivot")
+
+    #Curve Squash Offset
+    cmds.curve(p=[(0,0,0), (0,height,0)], degree=1, name="Crv_Squash_Offset")
+    cmds.parent("Crv_Squash_Offset", f"Ribbon_Spine_{name}|ExtraNodes_01|Grp_Crvs")
+    CurveSquashOffset = cmds.listRelatives("Crv_Squash_Offset", s=True)
+    cmds.shadingNode("attachCurve", au=True, name="attachCurve_Ribbon")
+    cmds.shadingNode("rebuildCurve", au=True, name="rebuildCurve_Ribbon")
+    cmds.setAttr("rebuildCurve_Ribbon.spans", 6)
+    Bookmark.addNodeToBookmark("node_Ribbon_Spine", CurveSquashOffset[0], row=-2, column=0, state=1)
+    Bookmark.addNodeToBookmark("node_Ribbon_Spine", InitLenght, row=-2, column=-1, state=1)
+    Bookmark.addNodeToBookmark("node_Ribbon_Spine", SquashChest, row=-2, column=-1, state=1)
+    Bookmark.addNodeToBookmark("node_Ribbon_Spine", "attachCurve_Ribbon", row=-1, column=1, state=1)
+    Bookmark.addNodeToBookmark("node_Ribbon_Spine", "rebuildCurve_Ribbon", row=-1, column=2, state=1)
+
+    cmds.connectAttr(SquashChest + ".worldPosition[0]", CurveSquashOffset[0] + ".controlPoints[0]")
+    cmds.connectAttr(InitLenght + ".worldPosition[0]", CurveSquashOffset[0] + ".controlPoints[1]")
+
+    cmds.connectAttr(CrvShape[0] + ".worldSpace[0]", "attachCurve_Ribbon.inputCurve1")
+    cmds.connectAttr(CurveSquashOffset[0] + ".worldSpace[0]", "attachCurve_Ribbon.inputCurve2")
+
+    cmds.connectAttr("attachCurve_Ribbon.outputCurve", "rebuildCurve_Ribbon.inputCurve")
+
+    #region FK CTRLs
+    CtrlUpperBody = Controllers.createController("3D_Shapes/boat", "CTRL_UpperBody")
+    Offset.Offset(CtrlUpperBody, nbr=1)
+    CtrlFKMid = cmds.circle(center=[0,0,0], nr=[0,1,0], radius=height/2.5, name="CTRL_FK_Mid")
+    Offset.Offset(CtrlFKMid, nbr=1)
+    CtrlFKChest = Controllers.createController("3D_Shapes/corner", "CTRL_FK_Chest")
+    Offset.Offset(CtrlFKChest, nbr=1)
+
+    #Hierarchy
+    cmds.parent(CtrlUpperBody + "_Offset", "CTRLs_01")
+    cmds.parent(CtrlFKMid + "_Offset", CtrlUpperBody)
+    cmds.parent(CtrlFKChest + "_Offset", CtrlFKMid)
+    cmds.parent(CTRLIK + "_Offset", CtrlFKChest)
+
+    #region Loc Axis mid Spine Info
+    LocAxisMidSpine = cmds.spaceLocator(n="Loc_Axis_Mid_Spine_Info")[0]
+    Offset.Offset(LocAxisMidSpine, nbr=2)
+
+    cmds.parent(LocAxisMidSpine + "_Offset", "Grp_Locs")
+
+    #Connection
+    cmds.shadingNode("addDoubleLinear", au=True, name="add_tx_Mid_Chest")
+    cmds.shadingNode("addDoubleLinear", au=True, name="add_ty_Mid_Chest")
+    cmds.shadingNode("addDoubleLinear", au=True, name="add_tz_Mid_Chest")
+    cmds.shadingNode("multDoubleLinear", au=True, name="mult_scaleY_Mid_UpperBody")
+
+    Bookmark.createBookmark("node_Ribbon_Spine_LocAxisMidSpine")
+    Bookmark.addNodeToBookmark("node_Ribbon_Spine_LocAxisMidSpine", LocAxisMidSpine, state=2)
+    Bookmark.addNodeToBookmark("node_Ribbon_Spine_LocAxisMidSpine", CtrlFKMid + "_Offset", row=-1, state=1)
+    Bookmark.addNodeToBookmark("node_Ribbon_Spine_LocAxisMidSpine", CtrlFKChest + "_Offset",row=-2 ,state=1)
+    Bookmark.addNodeToBookmark("node_Ribbon_Spine_LocAxisMidSpine", CtrlUpperBody, row=-3, state=1)
+    Bookmark.addNodeToBookmark("node_Ribbon_Spine_LocAxisMidSpine", "add_tx_Mid_Chest", row=-1, column=1, state=1)
+    Bookmark.addNodeToBookmark("node_Ribbon_Spine_LocAxisMidSpine", "add_ty_Mid_Chest", row=-2, column=1, state=1)
+    Bookmark.addNodeToBookmark("node_Ribbon_Spine_LocAxisMidSpine", "add_tz_Mid_Chest", row=-3, column=1, state=1)
+
+
+
+
+
+
+
 
     
 ColumnRibbon("01")
